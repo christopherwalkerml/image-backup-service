61d6d2854edbd707956056d013031e55144326ca
christopherwalkerml <christopherwalkerml@gmail.com>
2022-06-07 21:46:23 -0400

    gui works with images and videos. will add user input for merge folders and deletion of files

--- clean_up_images.py
+++ clean_up_images.py
@@ -5,14 +5,14 @@ import imagehash, os, cv2, tkinter
 
 img_map = {}
 root = Tk()
-root.geometry("900x700")
+root.geometry("1140x700")
 runner = 0
 
 grid = [[None, None]]
 
 def run():
     global runner
-    Label(root, text='Choose an image', font=("Ariel", 25)).grid(row=0, column=0, columnspan=2, pady=20)
+    Label(root, text='Choose an image to keep', font=("Ariel", 25)).grid(row=0, column=0, columnspan=2, pady=20)
     
     for folder in ['Iphone 6 - 12-14-2020']:
         print(folder)
@@ -36,7 +36,7 @@ def run():
                 data = img_map[hashed]
                 print(folder + '\\' + file + '  ==  ' + data[0] + '\\' + data[1])
 
-                global_runner = 0
+                runner = 0
                 update_gui(folder, file, data[0], data[1])
 
 
@@ -47,18 +47,16 @@ def save_vid_as_img(folder, filename):
 
 
 def update_gui(folder_1, file_1, folder_2, file_2):
-    img_1 = Image.open(f'{folder_1}\\{file_1}')
-    img_2 = Image.open(f'{folder_2}\\{file_2}')
-    img_1 = img_1.resize((300, 300))
-    img_2 = img_2.resize((300, 300))
-    photoimage_1 = ImageTk.PhotoImage(img_1)
-    photoimage_2 = ImageTk.PhotoImage(img_2)
+    img_1 = open_tk_img(folder_1, file_1)
+    img_2 = open_tk_img(folder_2, file_2)
 
-    Button(root, image = photoimage_1, command=lambda: del_file(folder_1, file_1)).grid(row=1, column=0, padx=30)
-    Button(root, image = photoimage_2, command=lambda: del_file(folder_2, file_2)).grid(row=1, column=1, padx=30)
+    Button(root, image = img_1, command=lambda: del_file(folder_1, file_1)).grid(row=1, column=0, padx=30)
+    Button(root, image = img_2, command=lambda: del_file(folder_2, file_2)).grid(row=1, column=1, padx=30)
 
-    Label(root, text=f'{folder_1}\\{file_1}').grid(row=2, column=0)
-    Label(root, text=f'{folder_2}\\{file_2}').grid(row=2, column=1)
+    Label(root, text=f'{folder_1}\\{file_1}').grid(row=2, column=0, pady=20)
+    Label(root, text=f'{folder_2}\\{file_2}').grid(row=2, column=1, pady=20)
+
+    Button(root, text="Keep Both", command=lambda: reset_runner()).grid(row=3, column=0, columnspan=2)
 
     while runner == 0:
         root.update()
@@ -70,8 +68,20 @@ def del_file(folder, file):
     runner = 1
 
 
-def open_tk_img():
-    save_vid_as_img(folder, file)
-    return Image.open(folder + "\\temp_frame.jpg")
+def reset_runner():
+    global runner
+    runner = 1
 
+
+def open_tk_img(folder, file):
+    if '.mov' in file.lower() or '.mp4' in file.lower():
+        save_vid_as_img(folder, file)
+        img = Image.open(folder + "\\temp_frame.jpg")
+    else:
+        img = Image.open(f'{folder}\\{file}')
+        
+    img = img.resize((500, 500))
+    return ImageTk.PhotoImage(img)
+    
+    
 run()
